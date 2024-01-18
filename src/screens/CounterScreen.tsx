f11913fbdfc456e529042ac96993e45877366e2c
Abid Ali <abdsoftfsd@gmail.com>
2024-01-18 12:55:58 +0500

    Refactored the CounterScreen.tsx to use reducer

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,30 +1,30 @@
 import * as React from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
-import {useState} from 'react';
+import {useReducer} from 'react';
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'increase':
-      return state + action.payload;
+      return {...state, count: state.count + action.payload};
     case 'decrease':
-      return state - action.payload;
+      return {...state, count: state.count - action.payload};
     default:
       return state;
   }
 };
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {count: 0});
   const increaseCounter = () => {
-    setCounter(counter + 1);
+    dispatch({type: 'increase', payload: 1});
   };
   const decreaseCounter = () => {
-    setCounter(counter - 1);
+    dispatch({type: 'decrease', payload: 1});
   };
   return (
     <View style={styles.screen}>
       <Button title={'Increase'} onPress={increaseCounter} />
       <Button title={'Decrease'} onPress={decreaseCounter} />
-      <Text>Counter: {counter}</Text>
+      <Text>Counter: {state.count}</Text>
     </View>
   );
 };
