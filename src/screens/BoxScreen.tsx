75f74f691a252ac0cb9659e70597b261a1a8cdea
Abid Ali <abdsoftfsd@gmail.com>
2024-01-18 17:22:02 +0500

    Understanding the layout styling

--- src/screens/BoxScreen.tsx
+++ src/screens/BoxScreen.tsx
@@ -3,20 +3,36 @@ import {StyleSheet, View, Text} from 'react-native';
 
 const BoxScreen = () => (
   <View style={styles.viewStyle}>
-    <Text style={styles.textStyle}>Box Screen</Text>
+    <Text style={styles.textStyle1}>Child #1</Text>
+    <Text style={styles.textStyle2}>Child #2</Text>
+    <Text style={styles.textStyle3}>Child #3</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
   viewStyle: {
-    borderWidth: 1,
+    borderWidth: 3,
     borderColor: 'black',
+    height: 200,
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  textStyle: {
+  textStyle1: {
     borderWidth: 1,
     borderColor: 'red',
-    margin: 20,
-    padding: 20,
+  },
+  textStyle2: {
+    borderWidth: 1,
+    borderColor: 'red',
+    fontSize: 20,
+    flex: 1,
+    backgroundColor: 'orange',
+    ...StyleSheet.absoluteFillObject,
+  },
+  textStyle3: {
+    borderWidth: 1,
+    borderColor: 'red',
   },
 });
 
